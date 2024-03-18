09df102a27a8cb76e9273702cdda469aac73ce31
Lenah David <lenah.david@tracom.co.ke>
2024-03-18 08:14:35 +0300

    modifying of controllers

--- src/main/java/com/example/blogging/controllers/CommentController.java
+++ src/main/java/com/example/blogging/controllers/CommentController.java
@@ -4,6 +4,7 @@ import com.example.blogging.dto.CommentResponse;
 import com.example.blogging.entity.Comment;
 import com.example.blogging.service.BlogPostService;
 import com.example.blogging.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -25,12 +26,21 @@ public class CommentController {
 
     @Autowired
     BlogPostController blogPostController;
+    @Operation(
+            description = "Posting comments",
+            summary = "Posting of comments"
 
+    )
     @PostMapping("/comment/{id}")
     public CommentResponse saveComment(@RequestBody Comment comment, @PathVariable("id") Long id, HttpServletRequest request) {
         String username = blogPostController.getUsernameFromHeader(request);
         return commentService.createComment(comment, id, username);
     }
+    @Operation(
+            description = "Getting all comments",
+            summary = "Getting of comments with pagination"
+
+    )
 
     @GetMapping("/comment")
     public ResponseEntity<List<Comment>> getAllComments(
@@ -43,25 +53,41 @@ public class CommentController {
         Page<Comment> commentPage = commentService.getAllComments(pageable);
         return ResponseEntity.ok(commentPage.getContent());
     }
+    @Operation(
+            description = "Getting a comment",
+            summary = "Getting a comment by id"
 
+    )
     @GetMapping("/comment/{id}")
     public Optional<CommentResponse> getComment(@PathVariable Long id) {
         return commentService.getCommentById(id);
     }
+    @Operation(
+            description = "Editing a comment",
+            summary = "Editing a comment"
 
+    )
     @PutMapping("/comment/{id}")
     public CommentResponse updateComment(@PathVariable Long id, @RequestBody Comment updatedComment, HttpServletRequest request) {
         String username = blogPostController.getUsernameFromHeader(request);
         return  commentService.updateComment(id, updatedComment, username);
     }
+    @Operation(
+            description = "Deleting a comment",
+            summary = "Deleting of comments"
 
+    )
     @DeleteMapping("/comment/{id}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long id, HttpServletRequest request) {
         String username = blogPostController.getUsernameFromHeader(request);
         commentService.deleteCommentById(id, username);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    @Operation(
+            description = "Getting of all comments",
+            summary = "Getting of comments"
 
+    )
     @GetMapping("/comments")
     public ResponseEntity<List<CommentResponse>> getAllComments(){
         return ResponseEntity.ok(commentService.getComments());
